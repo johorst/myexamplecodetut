f9fa9a55495517938e84d3477e1d2d3e526adf4c
johorst <johorst@mailinator.com>
2013-03-21 15:02:34 +0100

    Update server.js
    schema für mongodb added

--- server.js
+++ server.js
@@ -16,7 +16,36 @@ app.configure(function () {
     app.use(express.static(path.join(application_root, "public"))); //Where to serve static content
     app.use(express.errorHandler({ dumpExceptions:true, showStack:true })); //Show all errors in development
 });
- 
+
+// Routes
+app.get('/api', function(req, res){
+    res.send('Library API is running');
+});
+
+//Get a list of all books
+app.get('/api/books', function (req, res) {
+    return BookModel.find(function (err, books) {
+        if (!err) {
+            return res.send(books);
+        } else {
+            return console.log(err);
+        }
+    });
+});
+
+//Connect to database
+mongoose.connect('mongodb://localhost/library_database');
+
+//Schemas
+var Book = new mongoose.Schema({
+    title:String,
+    author:String,
+    releaseDate:Date
+});
+//Models
+var BookModel = mongoose.model('Book', Book);
+
+
 //Start server
 app.listen(4711, function () {
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
